bc466b6758a44277c4b07b97e05e9e46a93a9dfe
comesm <comesm@gmail.com>
2016-12-12 15:51:25 -0800

    implement stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,56 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if(obj === undefined) {
+    return 'undefined';
+  }
+ 
+
+  if (typeof obj === 'object') {
+    if(obj === null) {
+      return 'null';
+    }
+
+    if (Array.isArray(obj))
+    {
+      let stringifiedArrayResult = '';
+
+      for (let i = 0; i < obj.length; i++) {
+        if(i !== 0)
+        {
+          stringifiedArrayResult += ',' + stringifyJSON(obj[i]);
+        }
+        else
+        {
+          stringifiedArrayResult += stringifyJSON(obj[i]);
+        }
+        //console.log('line25 ' + '[' + stringifiedArrayResult + ']');
+       
+      }
+
+    return '[' + stringifiedArrayResult + ']';
+      
+    }
+    else
+    {
+      let stringifiedObjResult = [];
+      
+      for(let key in obj) {
+        if ((typeof obj[key] !== 'function') && (obj[key] !== undefined)){
+          let currKeyValuePairString = stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+          stringifiedObjResult.push(currKeyValuePairString);
+        }
+      }
+
+      return '{' + stringifiedObjResult.toString() + '}';
+    }
+  }
+
+  return obj + '';
+
   // your code goes here
 };
